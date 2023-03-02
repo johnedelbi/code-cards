2a9ae8b0e77a8688570dada6424358f584fee827
John Edelbi <iceflaming3@gmail.com>
2023-03-02 16:32:53 +0100

    clean hint component

--- src/components/Hint.tsx
+++ src/components/Hint.tsx
@@ -1,4 +1,4 @@
-import { IonButton, IonButtons, IonContent, IonHeader, IonModal, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonButton, IonButtons, IonContent, IonHeader, IonModal, IonTitle, IonToolbar } from '@ionic/react';
 import { useState } from 'react';
 import './hint.css';
 
@@ -6,33 +6,28 @@ export const Hint: React.FC = () => {
     const [isOpen, setIsOpen] = useState(false);
 
     return (
-        <IonPage>
-            <IonHeader>
-                <IonToolbar>
-                    <IonTitle>Inline Modal</IonTitle>
-                </IonToolbar>
-            </IonHeader>
-            <IonContent className="ion-padding">
-                <IonButton expand="block" onClick={() => setIsOpen(true)}>
-                    need help ! click to get a hint
-                </IonButton>
-                <IonModal isOpen={isOpen}>
-                    <IonHeader>
-                        <IonToolbar>
-                            <IonTitle>Modal</IonTitle>
-                            <IonButtons slot="end">
-                                <IonButton onClick={() => setIsOpen(false)}>Close</IonButton>
-                            </IonButtons>
-                        </IonToolbar>
-                    </IonHeader>
-                    <IonContent className="ion-padding">
-                        <p>
-                            test hint
-                        </p>
-                    </IonContent>
-                </IonModal>
-            </IonContent>
-        </IonPage>
+
+        <IonContent>
+            <IonButton expand="block" size="large" onClick={() => setIsOpen(true)}>
+                need help ! click to get a hint
+            </IonButton>
+            <IonModal isOpen={isOpen}>
+                <IonHeader>
+                    <IonToolbar>
+                        <IonTitle>Hint!</IonTitle>
+                        <IonButtons slot="end">
+                            <IonButton onClick={() => setIsOpen(false)}>Close</IonButton>
+                        </IonButtons>
+                    </IonToolbar>
+                </IonHeader>
+                <IonContent className="ion-padding">
+                    <p>
+                        the hint details will be here
+                    </p>
+                </IonContent>
+            </IonModal>
+        </IonContent>
+
     );
 };
 
