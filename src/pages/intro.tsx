93ce20c9d74ad2e9ee9e532d685dceca0d30430d
John Edelbi <iceflaming3@gmail.com>
2023-03-02 15:08:39 +0100

    add some improvment to the icon btn

--- src/pages/intro.tsx
+++ src/pages/intro.tsx
@@ -11,7 +11,7 @@ export const Intro: React.FC = () => {
         Quiz game</h1>
       <IonButton routerLink='/game' size="large" className='start-btn'>Start</IonButton>
 
-      <p>check the design on Figma<IonButton size="large" className='icon-btn' href='https://www.figma.com/file/YC05QHHI8B1amHqsrHrYO7/code-card-app?node-id=1%3A11&t=6AOQupLSKEg7qTRg-1' target='_blank'><IonIcon slot="icon-only" icon={logoFigma}></IonIcon></IonButton></p>
+      <p>check the design on Figma<IonButton color="light" size="large" className='icon-btn' href='https://www.figma.com/file/YC05QHHI8B1amHqsrHrYO7/code-card-app?node-id=1%3A11&t=6AOQupLSKEg7qTRg-1' target='_blank'><IonIcon slot="icon-only" icon={logoFigma}></IonIcon></IonButton></p>
 
       <p>check source code on github <IonButton color="light" size="large" className='icon-btn' href='https://github.com/johnedelbi/code-cards' target='_blank'><IonIcon slot="icon-only" icon={logoGithub}></IonIcon></IonButton></p>
     </IonContent>
