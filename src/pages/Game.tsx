4af33dcc6b616dcb81d546967b90b7194f4a4e82
John Edelbi <iceflaming3@gmail.com>
2023-03-02 16:33:36 +0100

    add answers option btn to game scene

--- src/pages/Game.tsx
+++ src/pages/Game.tsx
@@ -1,18 +1,15 @@
-import { IonContent, IonItem, IonLabel, IonPage } from '@ionic/react';
+import { IonButton, IonContent, IonPage } from '@ionic/react';
 import { Hint } from '../components/Hint'
 import './game.css';
 
 export const Game: React.FC = () => {
     return (
         <IonPage>
-            <IonContent fullscreen>
-                <IonItem>
-                    <IonLabel>
-                        <h1>Game</h1>
-                        <p>Game</p>
-
-                    </IonLabel>
-                </IonItem>
+            <IonContent className="ion-padding" fullscreen>
+                <IonButton className='answer-btn' size="large" expand='block'>option 1</IonButton>
+                <IonButton className='answer-btn' size="large" expand='block'>option 2</IonButton>
+                <IonButton className='answer-btn' size="large" expand='block'>option 3</IonButton>
+                <IonButton className='answer-btn' size="large" expand='block'>option 4</IonButton>
                 <Hint />
             </IonContent>
         </IonPage>
