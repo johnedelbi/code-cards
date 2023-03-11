4a6324c7eaf32fcfe2156f558cc16e8b4f80b027
John Edelbi <iceflaming3@gmail.com>
2023-03-11 16:30:15 +0100

    add game play logic

--- src/pages/Game.tsx
+++ src/pages/Game.tsx
@@ -1,29 +1,61 @@
-import { IonButton, IonCard, IonCardContent, IonCardHeader, IonCardTitle, IonContent, IonPage } from '@ionic/react';
+import { IonButton, IonContent, IonPage } from '@ionic/react';
 
 import { CodeCard } from '../components/CodeCard'
 import { Hint } from '../components/Hint'
+import { Result } from '../components/Result';
+
+import question from '../question.json';
+import React, { useState } from 'react';
 
 import './game.css';
 
 export const Game: React.FC = () => {
+    const [currentQuestion, setCurrentQuestion] = useState(0);
+    const [score, setScore] = useState(0);
+    const [showScore, setShowScore] = useState(false);
+
+    const answerClick = (isCorrect: boolean) => {
+        if (isCorrect) {
+            setScore(score + 1);
+        }
+
+        const nextQuestion = currentQuestion + 1;
+        if (nextQuestion < question.length) {
+            setCurrentQuestion(nextQuestion);
+        } else {
+            setShowScore(true);
+        }
+    };
+
+    const tryAgain = () => {
+        setCurrentQuestion(0);
+        setScore(0);
+        setShowScore(false);
+    };
+
     return (
         <IonPage>
-            <IonContent className="ion-padding" fullscreen>
-                <IonCard>
-                    <CodeCard />
-                    <IonCardHeader>
-                        <IonCardTitle>question 1</IonCardTitle>
-                    </IonCardHeader>
-
-                    <IonCardContent>
-                        Here's a small text description for the card content. Nothing more, nothing less.
-                    </IonCardContent>
-                </IonCard>
-                <IonButton className='answer-btn' size="large" expand='block'>option 1</IonButton>
-                <IonButton className='answer-btn' size="large" expand='block'>option 2</IonButton>
-                <IonButton className='answer-btn' size="large" expand='block'>option 3</IonButton>
-                <IonButton className='answer-btn' size="large" expand='block'>option 4</IonButton>
-                <Hint />
+            <IonContent>
+                {showScore ? (
+                    <Result score={score} totalQuestions={question.length} onTryAgain={tryAgain} />
+                ) : (
+                    <>
+                        <CodeCard code={question[currentQuestion].code}
+                            question={question[currentQuestion].question} />
+                        <div className='quiz-options'>
+                            {question[currentQuestion].options.map((option, index) => (
+                                <IonButton className='answer-btn' size="large" expand='block'
+                                    key={index}
+                                    onClick={() =>
+                                        answerClick(index === question[currentQuestion].answer)
+                                    }>
+                                    {option}
+                                </IonButton>
+                            ))}
+                        </div>
+                        <Hint hint={question[currentQuestion].hint} />
+                    </>
+                )}
             </IonContent>
         </IonPage>
     );
